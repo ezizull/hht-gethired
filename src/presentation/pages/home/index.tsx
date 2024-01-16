5c57eeb05f51481668cbacff33c0c07ae27c8ddf
ezizull <mfiqor@gmail.com>
2024-01-17 06:28:46 +0700

    fix responsive list products

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -25,7 +25,7 @@ export default function HomePage() {
       {
         // Products
         products && (
-          <ScrollArea className='w-[49rem] h-[46rem] max-h-[46rem]'>
+          <ScrollArea className='w-full sm:w-96 md:w-[49rem] h-[46rem] max-h-[46rem]'>
             <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
               {/* Odd */}
               <section className='grid auto-rows-min gap-4'>
